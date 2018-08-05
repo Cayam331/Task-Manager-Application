d470f8cc8c0e692253648aa4830b23bcfd735c20
cayam331 <eryk.ku@interia.pl>
2018-08-05 12:07:55 +0200

    Controller methods

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -2,12 +2,22 @@ package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
+@Transactional
+@Repository
 public interface TaskRepository extends CrudRepository<Task, Integer> {
     @Override
     List<Task> findAll();
 
-    Task findById(Long taskId);
+    Optional<Task> findById(Long id);
+
+    @Override
+    Task save(Task task);
+
+    void deleteById(Long id);
 }
